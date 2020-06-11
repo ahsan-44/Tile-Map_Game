f5c4d68d7c49f3da1ad690a8d2477871f9c1a06d
ahsan-44 <58401807+ahsan-44@users.noreply.github.com>
2020-06-12 02:45:15 +0500

    Main

--- TileMap_Game/Assets/Scripts/Player.cs
+++ TileMap_Game/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -8,6 +9,7 @@ public class Player : MonoBehaviour
    public float speed = 3f;
     public float jump = 2f;
     public bool grounded = false;
+   // private bool resetGroundedNeeded = false;
 
 
     void Start()
@@ -25,9 +27,17 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && grounded == true)
         {
-            rigid.velocity = new Vector2(rigid.velocity.x, jump);
+            rigid.velocity = new UnityEngine.Vector2(rigid.velocity.x, jump);
             grounded = false;
         }
-        rigid.velocity = new Vector2(horizontalInput * speed, rigid.velocity.y);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, UnityEngine.Vector2.down, 0.6f, 1 << 8);
+        Debug.DrawLine(transform.position, UnityEngine.Vector2.down * 0.6f, Color.red);
+         if(hitInfo.collider != null)
+        {
+            grounded = true;
+        }
+
+        rigid.velocity = new UnityEngine.Vector2(horizontalInput * speed, rigid.velocity.y);
     }
 }
