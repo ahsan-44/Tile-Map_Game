a97e9860b1573f3f2ffbb8b53970122bdc84dc88
ahsan-44 <58401807+ahsan-44@users.noreply.github.com>
2020-06-18 23:51:29 +0500

    Player.cs

--- TileMap_Game/Assets/Scripts/Player.cs
+++ TileMap_Game/Assets/Scripts/Player.cs
@@ -9,24 +9,42 @@ public class Player : MonoBehaviour
    public PlayerAnimation _anim;
    public float speed = 3f;
    public bool grounded = false;
-   public float jumpForce = 10f;
+   public float jumpForce = 6f;
+   public bool resetJumpNeeded = false;
+   private SpriteRenderer sr;
 
     void Start() 
   {
       rb = GetComponent<Rigidbody2D>();
       _anim = GetComponent<PlayerAnimation>();  
+      sr = GetComponentInChildren<SpriteRenderer>();
   }
 
 void Update()
 {
     Movement();
+    CheckGrounded();
 }
 public void Movement()
 {
     float move = Input.GetAxisRaw("Horizontal");
+
+    if(move > 0)
+    {
+        sr.flipX = true;
+    }
+    else if(move < 0)
+    {
+        sr.flipX = false;
+    }
+
+
    if(Input.GetKeyDown(KeyCode.Space) && grounded== true)
    {
        rb.velocity = new UnityEngine.Vector2(rb.velocity.x, jumpForce);
+       grounded = false;
+       resetJumpNeeded = true;
+       StartCoroutine(ResetJumpCall());
        
    }
 
@@ -34,5 +52,26 @@ public void Movement()
     _anim.Move(move);
 
 }
+
+void CheckGrounded()
+{
+    RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, UnityEngine.Vector2.down, 0.6f, 1<<8);
+    Debug.DrawLine(transform.position,UnityEngine.Vector2.down * 0.6f, Color.blue);
+
+    if(hitInfo.collider != null)
+    {
+        Debug.Log("Something hiting" + hitInfo.collider.name);
+        if(resetJumpNeeded == false)
+        {
+             grounded = true;
+        }
+    }
+
+}
+ IEnumerator ResetJumpCall()
+ {
+     yield return new WaitForSeconds(0.1f);
+     resetJumpNeeded = false;
+ }
 
 }
